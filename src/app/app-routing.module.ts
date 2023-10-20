8040f0956335f906f6fc65c66ca076bec5575ea0
MethmaPeiris99 <methma34@gmail.com>
2023-10-20 21:16:32 +0530

    Define routes inside user component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { AboutComponent } from './components/about/about.component';
-import { CommunityComponent } from './components/community/community.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignUpComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'community', component: CommunityComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path:'user', loadChildren:() => import('./modules/user/user.module').then((m) => m.UserModule)},
   { path: '**', component: NotFoundComponent }
 ];
 
