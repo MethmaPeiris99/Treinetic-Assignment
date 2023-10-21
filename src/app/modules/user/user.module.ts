6c0762ce9d64382e25fd8d6a9035a4c29fb7c796
MethmaPeiris99 <methma34@gmail.com>
2023-10-21 11:52:11 +0530

    Import reactive form module

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserContainerComponent } from './components/user-container/user-container.component';
+import { AddRecipeModalComponent } from './components/add-recipe-modal/add-recipe-modal.component';
+import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
 
 
 @NgModule({
@@ -13,11 +16,14 @@ import { UserContainerComponent } from './components/user-container/user-contain
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    UserContainerComponent
+    UserContainerComponent,
+    AddRecipeModalComponent,
+    RecipeCardComponent
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
