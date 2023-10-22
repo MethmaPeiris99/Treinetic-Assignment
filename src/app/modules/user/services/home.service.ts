7e0c46710b83242ce737a7abd550980386a824f2
MethmaPeiris99 <methma34@gmail.com>
2023-10-22 10:00:27 +0530

    Implement the functionality to display the recipes

--- src/app/modules/user/services/home.service.ts
+++ src/app/modules/user/services/home.service.ts
@@ -5,8 +5,11 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class HomeService {
+  
   private modalOpenSubject = new BehaviorSubject<boolean>(false);
   modalOpen = this.modalOpenSubject.asObservable();
+
+  isModalOpen: boolean[] = Array(12).fill(false);
 
   openModal() {
     this.modalOpenSubject.next(true);
@@ -14,5 +17,9 @@ export class HomeService {
 
   closeModal() {
     this.modalOpenSubject.next(false);
+  }
+
+  openRecipeModal(index:number) {
+    this.isModalOpen[index] = true
   }
 }
