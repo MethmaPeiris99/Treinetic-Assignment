e0b11cec3e7f10354a8cf1ceb9b82fcfea06d4c2
MethmaPeiris99 <methma34@gmail.com>
2023-10-22 18:42:26 +0530

    Create a unit test case for recipe list

--- src/app/modules/user/components/home/home.component.spec.ts
+++ src/app/modules/user/components/home/home.component.spec.ts
@@ -1,21 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { HomeService } from '../../services/home.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let homeService: HomeService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent],
+      providers: [HomeService],
+      imports: [HttpClientTestingModule],
     });
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    homeService = TestBed.inject(HomeService);
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Test case to check whether the recipeList is not empty when the HomeComponent is initially created
+  it('should initially have a non-empty recipeList', () => {
+    expect(component.recipeList).not.toBeNull();
+    expect(component.recipeList.length).toBeGreaterThan(0);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 });
