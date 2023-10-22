5b3468ce0af7ce478aff8dcf5732d93e67ba2c76
MethmaPeiris99 <methma34@gmail.com>
2023-10-22 18:06:19 +0530

    Add validations to the add recipe form

--- src/app/modules/user/components/add-recipe-modal/add-recipe-modal.component.ts
+++ src/app/modules/user/components/add-recipe-modal/add-recipe-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeService } from '../../services/home.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-recipe-modal',
@@ -15,12 +15,29 @@ export class AddRecipeModalComponent {
   }
 
   add_recipe_form = new FormGroup({
-    recipe_name: new FormControl(''),
-    recipe_ingridients: new FormControl(''),
-    recipe_steps: new FormControl(''),
+    recipe_name: new FormControl('', [Validators.required]),
+    recipe_ingridients: new FormControl('', [Validators.required]),
+    recipe_steps: new FormControl('', [Validators.required]),
+    prepTime: new FormControl('', [Validators.required]),
   });
 
   addRecipe() {
-    console.log(this.add_recipe_form.value)
+    console.log(this.add_recipe_form.value);
+  }
+
+  get recipe_name() {
+    return this.add_recipe_form.get('recipe_name');
+  }
+
+  get recipe_ingridients() {
+    return this.add_recipe_form.get('recipe_ingridients');
+  }
+
+  get recipe_steps() {
+    return this.add_recipe_form.get('recipe_steps');
+  }
+
+  get prepTime() {
+    return this.add_recipe_form.get('prepTime');
   }
 }
