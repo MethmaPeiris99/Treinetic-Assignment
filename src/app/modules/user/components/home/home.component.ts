b5e46b2969fae5ca761c83a13cc66b59f07d04b9
MethmaPeiris99 <methma34@gmail.com>
2023-10-22 09:59:22 +0530

    Add a bg image and display recipes

--- src/app/modules/user/components/home/home.component.ts
+++ src/app/modules/user/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeService } from '../../services/home.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,24 @@ import { HomeService } from '../../services/home.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  constructor(public homeService: HomeService) {}
+
+  recipeList: any;
+
+  constructor(public homeService: HomeService, public httpClient: HttpClient) {
+    this.recipeList = []
+  }
 
   openModal() {
     this.homeService.openModal();
+  }
+
+  ngOnInit():void {
+    this.getRecipeInfo()
+  }
+
+  getRecipeInfo() {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/posts').subscribe((result: any) => {
+      this.recipeList = result
+    });
   }
 }
