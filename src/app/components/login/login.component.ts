a96e68c3d29b0d3a95ff2ed62214c5eb65e72f22
MethmaPeiris99 <methma34@gmail.com>
2023-10-22 17:11:29 +0530

    Add validations to the login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,39 +1,45 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor(private authService: AuthService, private router: Router) {
-  }
-
-  ngOnInit(): void{
+  ngOnInit(): void {
     if (this.authService.isLoggedIn()) {
-      this.router.navigate(['user'])
+      this.router.navigate(['user']);
     }
   }
 
   loginForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl('')
-  })
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
 
-  onSubmit(): void{
+  onSubmit(): void {
     if (this.loginForm.valid) {
       this.authService.login(this.loginForm.value).subscribe(
         (result) => {
-          this.router.navigate(['user'])
+          this.router.navigate(['user']);
         },
         (error: Error) => {
-          alert(error.message)
+          alert(error.message);
         }
-      )
+      );
     }
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
   }
 }
